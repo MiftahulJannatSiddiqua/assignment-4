b03ee51d69d0778cb2029058e0fce54aa80ab82e
MiftahulJannatSiddiqua <mjsmifta7@gmail.com>
2022-08-12 16:22:38 +0600

    added

--- problem1.js
+++ problem1.js
@@ -1,1 +1,12 @@
 // ==============problem-1==========
+function radianToDegree(radian){
+    if(typeof radian=='number'){
+        degree = radian*(180/Math.PI);
+        return degree.toFixed(2);
+    } 
+    else{
+        return 'please try with a valid number!';
+    }  
+}
+let radianAndDegree= radianToDegree(10);
+console.log(radianAndDegree);
