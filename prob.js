b2ee1008dbfa7f709f6ddaa9087dd79b569e78d8
MiftahulJannatSiddiqua <mjsmifta7@gmail.com>
2022-08-12 17:35:32 +0600

    add

--- prob.js
+++ prob.js
@@ -1,10 +1,38 @@
-function radianToDegree(radian) {
-    if (typeof radian === 'number') {
-        const degree = radian * 180 / Math.PI;
-        return degree.toFixed(2);
-    } else {
-        return "Please input a valid Number!";
+// function radianToDegree(radian) {
+//     if (typeof radian === 'number') {
+//         const degree = radian * 180 / Math.PI;
+//         return degree.toFixed(2);
+//     } else {
+//         return "Please input a valid Number!";
+//     }
+// }
+// var givenRadian = radianToDegree();
+// console.log(givenRadian);
+function oilPrice(disel, petrol, octane) {
+    if (typeof (disel && petrol && octane) === 'number') {
+        let diselPrice = disel * 114;
+        let petrolPrice = petrol * 130;
+        let octanePrice = octane * 135;
+        let totalPrice = diselPrice + petrolPrice + octanePrice;
+        return totalPrice;
+    }
+    else {
+        return "Please input Quantity";
     }
 }
-var givenRadian = radianToDegree();
-console.log(givenRadian);
+let total = oilPrice(5, 5, 4);
+console.log(total);
+// function oilPrice(disel, petrol, octane) {
+//     if (typeof disel === 'number' && typeof petrol === 'number' && typeof octane === 'number') {
+//         let diselPrice = disel * 114;
+//         let petrolPrice = petrol * 130;
+//         let octanePrice = octane * 135;
+//         let totalPrice = diselPrice + petrolPrice + octanePrice;
+//         return totalPrice;
+//     }
+//     else {
+//         return "Please input Quantity";
+//     }
+// }
+// let total = oilPrice(5, 5, 4);
+// console.log(total);
--- problem3.js
+++ problem3.js
@@ -1,2 +1,17 @@
 // ============== problem-3 =============
 // %%%%%%%%%%% radianToDegree %%%%%%%%%%%
+
+function oilPrice(diesel, petrol, octane){
+    if (typeof (diesel && petrol && octane) == 'number'){
+        const dieselPrice = diesel * 114;
+        const petrolPrice = petrol * 130;
+        const octanePrice = octane * 135;
+        const totalPrice = dieselPrice + petrolPrice + octanePrice;
+        return totalPrice;
+    }
+    else{
+        return 'sorry! please try with quantity';
+    }
+}
+const priceOfOil = oilPrice(0, 2, 3);
+console.log(priceOfOil);
